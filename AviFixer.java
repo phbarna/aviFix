0dcc3b569509b69bb64262fda7a1141e219ee2c0
Pete <phbarna@tiscali.co.uk>
2022-05-06 23:16:17 +0100

    Use equalsIgnorecase for checking divx, or xvid instead of silly loop

--- AviFixer.java
+++ AviFixer.java
@@ -5,58 +5,60 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.*;
 
 public class AviFixer {
-
+    private static HashSet<String> hs = new HashSet<>();
     public static void main(String... args) {
-        File[] files = new File("D:/").listFiles();
+        File[] files = new File("E:").listFiles();
         if (files != null)
-            getFiles(files);
+            try {
+                getFiles(files);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(hs.size());
+            for (String s : hs) {
+                System.out.println(s);
+            }
     }
 
-    public static boolean checkForDivX(File file) {
+    public static boolean checkForDivX(File file) throws IOException {
 
         // getBytes from anyWhere
         // I'm getting byte array from File
+        FileInputStream fileStream = null;
         try {
-            FileInputStream fileStream = new FileInputStream(file);
+            fileStream = new FileInputStream(file);
 
             // Instantiate array
-            byte[] arr = new byte[(int) 150];
+            byte[] arr = new byte[(int) 120];
 
             // read All bytes of File stream
             fileStream.read(arr, 0, arr.length);
-            StringBuffer sb = new StringBuffer();
-            int count = 0;
-            for (int X : arr) {
 
-                sb.append((char) X);
-                if (count == 0 && ((char) X == 'X' || (char) X == 'x')) {
-                    count++;
-                } else if (count == 1 && ((char) X == 'V' || (char) X == 'v')) {
-                    count++;
-                } else if (count == 2 && ((char) X == 'I' || (char) X == 'i')) {
-                    count++;
-                } else if (count == 3 && ((char) X == 'D' || (char) X == 'd')) {
-                    return true;
-                } else {
-                    count = 0;
-                }
+            String format = new String(arr);
+            format = format.substring(112, 116);
+
+            if (format.equalsIgnoreCase("divx") || format.equalsIgnoreCase("xvid")) {
+                return true;
             }
+            fileStream.close();
             return false;
-            // System.out.print((char) X);
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
+
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
             fileStream.close();
         }
-
+        fileStream.close();
         return false;
     }
 
     public static void getFiles(File[] files) {
+    try {
         if (files == null) {
             return;
         }
@@ -67,12 +69,21 @@ public class AviFixer {
             } else {
                 if (file.getName().endsWith("avi")) {
                     if (checkForDivX(file)) {
-                        System.out.println(file.getAbsolutePath());
+
+                        String s = file.getAbsolutePath();
+                        int index = s.lastIndexOf("\\");
+                        String folder = s.substring(0, index);
+                        hs.add(folder);
 
                     }
                 }
             }
         }
+
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+
 
     }
 }
